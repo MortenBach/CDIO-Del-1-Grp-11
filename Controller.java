701b70bf6fce55eb64741f14eb20227d812f0093
Evilwerner <Kristopuls@gmail.com>
2016-10-04 10:42:31 +0200

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,4 +1,4 @@
-package dicerollpointsandsum;
+package terningdone;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -6,10 +6,10 @@ import java.util.Scanner;
 
 public class Controller {
 	
-	//Instance variabler skal være private
+	//Instance variabler skal vÃ¦re private
 	
 	private Dice die = new Dice();			// Laver et nyt objekt af klassen Dice
-	private int roll1 = 0;	 				// Laver to lokale variable, der indeholder to separate værdier
+	private int roll1 = 0;	 				// Laver to lokale variable, der indeholder to separate vÃ¦rdier
 	private int roll2 = 0;					// af die.roll();
 	private int diceEyes;					// Laver en diceEyes variabel, vi bruger i game() metoden.
 	
@@ -20,48 +20,48 @@ public class Controller {
 	private List<Player> Players = new ArrayList<Player>(); 		//Laver en Arrayliste, der kan indeholde referencer til objekter af Player
 	private Player tempPlayer;
 	
-	
+	private boolean checkSixes = false;	
 	private boolean win = false;									//Laver et boolean udtryk, vi bruger i vores game conditions												
 	
 	public Controller() { 
 	
-		Player1 = new Player("Player 1"); 		// Laver et objekt af klassen layer og sætter Player 1/2 ind som string i constructoren
+		Player1 = new Player("Player 1"); 		// Laver et objekt af klassen layer og sÃ¦tter Player 1/2 ind som string i constructoren
 		Player2 = new Player("Player 2");
-		Players.add(Player1);					// Tiføjer spillere til listen Players
+		Players.add(Player1);					// TifÃ¸jer spillere til listen Players
 		Players.add(Player2);
 		
 	}
 	
-	//Metoden, der kører spillet
+	//Metoden, der kÃ¸rer spillet
 		public void game() {
 		Scanner scan = new Scanner(System.in);	// Laver en scanner
 
-		while (! win) {							// Laver en while løkke, der kører forevigt da while (true)
+		while (! win) {							// Laver en while lÃ¸kke, der kÃ¸rer forevigt da while (true)
 
 			if (i < Players.size()) {													// Laver et if-statement til index i, i arraylisten Players
 				tempPlayer = Players.get(i);											// Laver en reference til klassen Player, vi kalder for tempPlayer. tempPlayer refererer til index i, i arrayListen Players														
 				System.out.println("It's " + tempPlayer + "'s turn to roll the dice"); 	// Printer
-				scan.nextLine();														// Venter på nextLine for at komme videre
+				scan.nextLine();														// Venter pÃ¥ nextLine for at komme videre
 				
-				roll1 = die.roll();														// Sætter roll1 og roll2 lig die.roll(), men på hver sin linie, så de får hver sin værdi
+				roll1 = die.roll();														// SÃ¦tter roll1 og roll2 lig die.roll(), men pÃ¥ hver sin linie, sÃ¥ de fÃ¥r hver sin vÃ¦rdi
 				roll2 = die.roll();
 				diceEyes = roll1 + roll2;												// Initialiserer diceEyes til roll1 + roll2
 				winDouble40();															// Benytter metoden winDouble40()
-				
+				seksere();
 					if(win) {															// Hvis win er true, stopper den while loopet
 						break;																	
 					}
 					
 			tempPlayer.setSum(roll1 + roll2);											// Opdaterer summen
-			
+			ettere();
 			System.out.println("Die 1: " + roll1 + "\nDie 2: " + roll2 + "\n" 			// Print statement
 					+ tempPlayer + " rolled a total of " + diceEyes + "\n" + tempPlayer 
 					+ " now has " + tempPlayer.getSum() + " points\n");
-
-				i++;																	// Inkrementerer i, hvorved vi nu får fat i den næste spiller i ArrayListen via Players.get(i)
+				
+				i++;																	// Inkrementerer i, hvorved vi nu fÃ¥r fat i den nÃ¦ste spiller i ArrayListen via Players.get(i)
 	
 				}
-			if (i == Players.size()) {													// if-statement, der resetter i når den bliver lig længden af Players listen
+			if (i == Players.size()) {													// if-statement, der resetter i nÃ¥r den bliver lig lÃ¦ngden af Players listen
 				i = 0;
 			}
 		}
@@ -71,12 +71,32 @@ public class Controller {
 	// Metoden til vores ene win condition
 	public void winDouble40() {			
 		if (roll1 == roll2) {
-			if (tempPlayer.getSum() >= 40) {
+			i--;
+			if (tempPlayer.getSum() >= 40 && roll1 != 1) {
 				System.out.println("Die 1: " + roll1 + "\nDie 2: " + roll2 + "\n" + tempPlayer
 						+ " has rolled a pair and has won the game.");
 				win = true;
 			}
 		}
 
+	}
+	public void seksere(){
+		if(roll1 == 6 && roll2 == 6){
+			if (checkSixes == true){
+				System.out.println(tempPlayer+" er vinder.");
+				win = true;
+				
+			}else{
+				checkSixes = true;
+			}
+		}
+		else{
+			checkSixes = false;
+		}
+	}
+	public void ettere(){
+		if (roll1 == 1 && roll2 == 1){
+			tempPlayer.setSum(-tempPlayer.getSum());
+		}
 	}
 }
